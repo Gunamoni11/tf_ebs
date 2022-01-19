ab9a8b98f130ea26e55b178a1233a0430a081dd2
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-19 18:09:50 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -10,4 +10,10 @@ resource "aws_elastic_beanstalk_environment" "tfenvtest3" {
   name                = "tf-test2"
   application         = aws_elastic_beanstalk_application.tftest3.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.9 running PHP 8.0"
+  
+    setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name = "IamInstanceProfile"
+      value = "demo_full_access"
+  }
 }
