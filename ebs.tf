6e1b58c18a8405eb9b0d873766eee2a2d8a0d61c
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-19 10:28:09 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_elastic_beanstalk_application" "tftest11" {
-  name        = "tf-test11"
+resource "aws_elastic_beanstalk_application" "tftest2" {
+  name        = "tf-test2"
   description = "tf-test-desc"
 }
 
-resource "aws_elastic_beanstalk_environment" "tfenvtest11" {
-  name                = "tf-test11"
-  application         = aws_elastic_beanstalk_application.tftest11.name
+resource "aws_elastic_beanstalk_environment" "tfenvtest2" {
+  name                = "tf-test2"
+  application         = aws_elastic_beanstalk_application.tftest2.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.9 running PHP 8.0"
 }
